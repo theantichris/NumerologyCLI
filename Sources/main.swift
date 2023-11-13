9fb5f1cdaaae00077e955e95f66efc5c27f7ba6f
Christopher Lamm <christopher.lamm@companycam.com>
2023-11-13 10:42:58 -0500

    refactoring and calculating the value of digits, still need to loop on this

--- Sources/main.swift
+++ Sources/main.swift
@@ -10,33 +10,55 @@ let groups:[String] = [
   "IR"
 ]
 
-var value = 0
+// TODO: print("Enter the words you want to calculate.\n")
 
-print("Enter the words you want to calculate.\n")
-
+// First pass 21, second pass 3
 var response = "Moon"
-var expected = 21
+
+// TODO: response = readLine()
 // TODO: strip out or show error for anything that isn't a letter.
-//response = readLine()
+
 response = response.uppercased()
 
-print("Response uppercased: \(response)")
+var value = getInitialValue(of: response)
 
-for letter in response {
-    print("Checking for letter: \(letter)")
+// TODO: Loop over value until we get a single digit number.
+
+if value > 9 {
+    let digits = getDigits(of: value)
+    value = 0 // Resetting the value to 0 for the new calculation.
     
-    for i in 1 ... groups.count {
-        let index = i - 1
-        
-        print("Checking in: \(groups[index])")
-        
-        if groups[index].contains(letter) {
-            print("Group \(groups[index]) does contain \(letter)")
-            value += i
-        } else {
-            print("Group \(groups[index]) does not contain \(letter))")
+    for digit in digits {
+        value += digit
+    }
+}
+
+func getInitialValue(of response: String) -> Int {
+    var calculation = 0
+    
+    for letter in response {
+        for i in 1 ... groups.count {
+            let index = i - 1
+            
+            if groups[index].contains(letter) {
+                calculation += i
+            }
         }
     }
+    
+    return calculation
+}
+
+func getDigits(of number: Int) -> [Int] {
+    var digits = [Int]()
+    var x = number
+    
+    repeat{
+        digits.insert(abs(x % 10), at: 0)
+        x /= 10
+    } while x != 0
+
+    return digits
 }
 
 print(value)
