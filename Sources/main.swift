abaf07be5a231c766c141c8dc25a619027a6c19c
Christopher Lamm <christopher.lamm@companycam.com>
2023-11-13 10:26:34 -0500

    calculating the initial value

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,4 @@
-let values:[String] = [
+let groups:[String] = [
   "AJS",
   "BKT",
   "CLU",
@@ -10,7 +10,33 @@ let values:[String] = [
   "IR"
 ]
 
+var value = 0
+
 print("Enter the words you want to calculate.\n")
 
-var response = ""
-response = readLine()!.uppercased()
+var response = "Moon"
+var expected = 21
+// TODO: strip out or show error for anything that isn't a letter.
+//response = readLine()
+response = response.uppercased()
+
+print("Response uppercased: \(response)")
+
+for letter in response {
+    print("Checking for letter: \(letter)")
+    
+    for i in 1 ... groups.count {
+        let index = i - 1
+        
+        print("Checking in: \(groups[index])")
+        
+        if groups[index].contains(letter) {
+            print("Group \(groups[index]) does contain \(letter)")
+            value += i
+        } else {
+            print("Group \(groups[index]) does not contain \(letter))")
+        }
+    }
+}
+
+print(value)
